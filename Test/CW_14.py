c131f20d9fed72dc0b5e2d5cf262c63ac2ff6c18
rabotaresp <36397055+rabotaresp@users.noreply.github.com>
2018-04-04 11:48:52 +0300

    Дополнил словами

--- Test/CW_14.py
+++ Test/CW_14.py
@@ -6,13 +6,31 @@
 # text = f.read()
 # print(text)
 # f.close()
-res_sp = []
-with open('file.txt') as f:
-    for line in f:
+# res_sp = []
+# with open('file.txt') as f:
+#     for line in f:
+#
+#         l = line.lower().split()
+#         for q in l:
+#             if q.endswith('.exe'):
+#                 res_sp.append(q)
+# for q in res_sp:
+#     print(q)
+stih = []
+def leng():
+    stih = []
+    with open('fail.txt') as f:
+        for l in f:
+            x = l.lower().split()
+            for q in x:
+                stih.append(q)
+        return (len(stih))
+print(leng())
+y = leng()
+def long_l():
+    for q in range(y):
+        print((stih.count(q)))
 
-        l = line.lower().split()
-        for q in l:
-            if q.endswith('.exe'):
-                res_sp.append(q)
-for q in res_sp:
-    print(q)
+
+
+print(long_l())
